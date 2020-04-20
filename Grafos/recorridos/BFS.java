ab978e7bdc0b676ab8d0c3bd29646e7fbe1cc0b6
Paul Landaeta <paulwilkerlf@gmail.com>
2020-04-19 22:15:46 -0400

    bfs in java

---
+++ Grafos/recorridos/BFS.java
@@ -0,0 +1,83 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BFS {
+	
+	
+	public static Scanner scanner = new Scanner(System.in);
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Read number of edges
+		int edges = scanner.nextInt();
+		
+		// Create a list with 'edges' edges. 
+		
+		LinkedList<Integer>[]  graph = new LinkedList[edges];
+		for(int i = 0; i < edges; i++) {
+			graph[i] = new LinkedList<Integer>();
+		}
+		// Read number of vertices
+		int vertices = scanner.nextInt();
+	
+		
+		// Read pair of edges. 
+		
+		for(int i = 0; i < vertices; i++ ) {
+			int edgeOne,edgeTwo;
+			edgeOne = scanner.nextInt();
+			edgeTwo = scanner.nextInt();
+			graph[edgeOne].add(edgeTwo);
+			graph[edgeTwo].add(edgeOne);
+		}
+		
+		//Print Graph 
+		for(int i = 0; i< edges; i++) {
+			System.out.print("Edge {"+i+"} => ");
+			for(int j = 0; j < graph[i].size(); j++ ) {
+				System.out.print("["+graph[i].get(j)+"]");
+			}	
+			System.out.println();
+		}
+		
+		//BFS 
+		//Create the queue to save the next edges. 
+		Queue<Integer> edgeQueue = new LinkedList<Integer>();
+ 		boolean[]	edgeVisited = new boolean[edges];
+ 		
+ 		//insert the first edge. 
+ 		edgeVisited[0] = true;
+ 		edgeQueue.add(0);
+ 		
+ 		while(!edgeQueue.isEmpty()) {
+ 			int currentEdge = edgeQueue.remove();
+ 			for(int i=0;i<graph[currentEdge].size();i++) {
+ 				if(!edgeVisited[graph[currentEdge].get(i)]) {
+ 					edgeVisited[graph[currentEdge].get(i)] = true;
+ 					edgeQueue.add(graph[currentEdge].get(i));
+ 				}
+ 			}
+ 		}
+ 		if(edgeVisited[2]) {
+ 			System.out.println("The edge 2 was visited");
+ 		}else {
+ 			System.out.println("The edge 2 was not visited");
+ 		}
+	}
+
+}
+
+/* 
+4 3
+0 1
+1 2
+3 1
+*/
+/* 
+4 2
+0 1
+1 3
+*/
