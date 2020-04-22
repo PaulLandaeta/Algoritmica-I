1852fc698e975a577dd8dfe4f6bdfed7fc3e350d
Paul Landaeta <paulwilkerlf@gmail.com>
2020-04-22 09:30:05 -0400

    fix

--- Grafos/recorridos/352.cpp
+++ Grafos/recorridos/352.cpp
@@ -38,7 +38,6 @@ int main() {
         for(int i=0;i<n;i++) {
             for(int j = 0; j<n; j++) {
                 if(graph[i][j]=='1') {
-                    map[graph[i][j]]++;
                    dfs(i,j);
                    cont2++;
                 }
